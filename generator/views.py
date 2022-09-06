b826e77106ff2d2b8eb5ee4101e51912bf6748bd
Toutchy kipik <toutchy.kipik@pm.me>
2022-09-06 02:06:19 +0200

    init suite 2

--- generator/views.py
+++ generator/views.py
@@ -11,29 +11,21 @@ def home(request):
         if site == "":
             return render(request, 'generator/home.html')
         password_length = int(request.POST.get('length'))
-        characters = "!@#$%^&**()_+"
-        numbers = '1234567890'
-        small_letters = "qwertyuioplkjhgfdsazxcvbnm"
-        upper_case = "QWERTYUIOPASDFGHJKLMNBVCXZ"
-        prep = characters + numbers + small_letters + upper_case
         if password_length > 30:
             message = "can't generate password more than 30 characters"
-            context = {
-                'message': message
-            }
+            context = {'message': message}
             return render(request, 'generator/home.html', context)
-
         else:
+            characters = "!@#$%^&**()_+"
+            numbers = '1234567890'
+            small_letters = "qwertyuioplkjhgfdsazxcvbnm"
+            prep = characters + numbers + small_letters + "QWERTYUIOPASDFGHJKLMNBVCXZ"
             passwd = ''.join(random.sample(prep, k=password_length))
             print(passwd)
             p = GenPass.objects.create(site=site, passwords=passwd)
             p.save()
-
-            context = {
-                'password': passwd
-            }
+            context = {'password': passwd}
             return render(request, 'generator/success.html', context)
-
     return render(request, "generator/home.html")
 
 
