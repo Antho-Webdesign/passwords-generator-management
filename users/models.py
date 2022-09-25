113d03f7aa1fab0a0981a059fc5a0a92c9fd184c
Toutchy kipik <toutchy.kipik@pm.me>
2022-09-25 22:11:03 +0200

    db + prof

--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+
+user = get_user_model()
 
 
 class Profile(models.Model):
