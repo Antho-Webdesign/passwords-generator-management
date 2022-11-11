ee7d4a732b349984786aae90f36ab827b585fdbc
Toutchy kipik <toutchy.kipik@pm.me.Toutchy kipik>
2022-11-11 11:07:11 +0100

    herok test

--- djangoPasswordGenerator/settings.py
+++ djangoPasswordGenerator/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
-    'staticfiles'
+    'static/'
 ]
 MEDIA_ROOT = 'media'
 MEDIA_URL = 'media/'
