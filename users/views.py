d07b6548e1c6ca10fc3a76ff835fd08cb18539cc
Toutchy kipik <toutchy.kipik@outlook.com>
2022-12-22 00:55:37 +0100

    ok

--- users/views.py
+++ users/views.py
@@ -36,22 +36,8 @@ def register(request):
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, 'Votre compte a été mis à jour !')
-
-            return redirect('profile')
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-    context = {'u_form': u_form, 'p_form': p_form}
-
-    return render(request, 'users/profile.html', context)
+    user = request.user
+    return render(request, 'users/profile.html', {'user': user})
 
 
 @login_required
