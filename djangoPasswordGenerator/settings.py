3bea5a81c345952ad349a9c1faf2f32ea2da633a
Toutchy kipik <toutchy.kipik@outlook.com>
2022-12-21 13:51:03 +0100

    init heroku

--- djangoPasswordGenerator/settings.py
+++ djangoPasswordGenerator/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path#
-# import dj_database_url
-# import django_heroku
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,4 +136,4 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
