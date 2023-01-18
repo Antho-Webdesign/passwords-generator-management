e1cb4041accd10f411a6795eeb840a696d331ce3
Antho-Webdesign <toutchyy.kipik@outlook.com>
2023-01-18 07:52:07 +0100

    refac ok

--- generator/admin.py
+++ generator/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from generator.models import GenPass
 
+
+class GenPassAdmin(admin.ModelAdmin):
+    list_display = ('user', 'site', 'time')
+    list_filter = ('user', 'site', 'time')
+    search_fields = ('user',)
+
+
 # Register your models here.
-admin.site.register(GenPass)
+admin.site.register(GenPass, GenPassAdmin)
+# Register genpass model
